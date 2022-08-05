b2d8572a4030f5c2931b3f60db15d0e25ad44cae
viniciusgit2 <4xv6patelfin@gmail.com>
2022-08-05 11:49:50 -0300

    primeira parte

--- next e react/base/megas/mega.js
+++ next e react/base/megas/mega.js
@@ -1,12 +1,14 @@
 
 
-  export function mega(qtd=6,numeros=[]){
-  if(qtd<6 && qtd>60){
+   function mega(qtd=6,numeros=[]){
+  if(qtd>6 && qtd<60){
     throw"impossivel"
-  }if(numeros.length === qtde){
+  }if(numeros.length ===qtd){
     return numeros
   }
-  
+  const numerosaletorios=parseInt(Math.random)*60+1
+  if(numeros.includes(numerosaletorios)){
+    return mega(qtd,{numeros,numerosaletorios})
   }
-  console.log
-  (mega())
+}
+  console.log(mega())
