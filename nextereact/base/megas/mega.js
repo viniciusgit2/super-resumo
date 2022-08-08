b0a914ca651b63db6822fe058f7e6a72aba13c39
viniciusgit2 <4xv6patelfin@gmail.com>
2022-08-08 18:03:52 -0300

    5 minutos no meu notebook

--- nextereact/base/megas/mega.js
+++ nextereact/base/megas/mega.js
@@ -1,14 +1,14 @@
 
  
-  function mega (qtd=6 ,numeros = {}){
+    function mega (qtd=6 ,numeros = {}){
   
   if(qtd<6 && qtd>60){
     throw"impossivel"
-  }if (numeros.length){
+  }if (numeros.length=qtd){
     return numeros
   }
 const numerosaletorios=parseInt({qtd}*60)+1
-  if(numeros.incluides(numerosaletorios)
+  if(numeros(numerosaletorios)
 )
   
 return mega (qtd,{...numerosaletorios,numeros})
