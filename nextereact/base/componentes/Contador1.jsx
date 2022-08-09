1f41fdbd449b843579755dd9a515430308968019
viniciusgit2 <4xv6patelfin@gmail.com>
2022-08-09 11:20:58 -0300

    9-08

---
+++ nextereact/base/componentes/Contador1.jsx
@@ -0,0 +1,7 @@
+import { Component } from "react";
+import contador from "../pages/classe/contador";
+export default class Contador extends Component{
+render(){
+return(<div> valor{100} </div>) 
+}
+}
---
+++ nextereact/base/pages/classe/contador.jsx
@@ -0,0 +1,7 @@
+import { Component } from "react"
+import Contador1 from "../../componentes/Contador1"
+export default class contador extends Component{
+render(){
+return(<div><h1> teste classe</h1>,<h2><Contador1 /></h2></div>)
+}
+}
